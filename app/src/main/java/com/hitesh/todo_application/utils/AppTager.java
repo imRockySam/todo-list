aa068564b6c230feac2b03bc76b2058d8ddcba21
Hitesh Sachdev <sachdev.hitesh0404@gmail.com>
2021-10-01 13:17:09 +0530

    PR for Java Files 5

---
+++ app/src/main/java/com/hitesh/todo_application/utils/AppTager.java
@@ -0,0 +1,17 @@
+package com.hitesh.todo_application.utils;
+
+
+public class AppTager {
+
+    public static String getTag() {
+        String tag = "";
+        final StackTraceElement[] ste = Thread.currentThread().getStackTrace();
+        for (int i = 0; i < ste.length; i++) {
+            if (ste[i].getMethodName().equals("getTag")) {
+                tag = "("+ste[i + 1].getFileName() + ":" + ste[i + 1].getLineNumber()+")";
+            }
+        }
+        return tag;
+    }
+
+}
